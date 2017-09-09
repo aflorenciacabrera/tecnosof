8fe049e4664889a9b8edd2c9a6fbc0e05384c871
Florencia Cabrera <aflorenciacabrera@gmail.com>
2017-09-09 18:11:26 -0300

    subi

---
+++ phpunit/src/Tests/CalculadoraTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+class CalculadoraTest extends \PHPUnit_Framework_TestCase {
+	
+
+	 public function testCalculadora() {
+
+	 	$calculadora = new Calculadora();
+
+        $this->assertEquals(10, $calculadora->sumar(4, 6));
+
+        $this->assertEquals(10, $calculadora->multiplicar(2, 5));
+
+        $this->assertEquals(24, $calculadora->factorial(4));
+
+    }
+}
